117fd2208e213477a9aed3002b72ecde292eebd1
Andy Reid <andyrmreid@gmail.com>
2022-08-04 09:59:00 -0400

    Sterilize populateMenu.js

--- public/js/populateMenus.js
+++ public/js/populateMenus.js
@@ -3,194 +3,117 @@ const jobTitleSelectMenu = document.getElementById('jobTitle');
 
 
 
-const compassRoseJobs = [
+const mainOfficeJobs = [
     {
-        value: 'crarft.template',
-        text: 'Compass Rose Family Teacher'
-    },
-    {
-        value: 'crteach.template',
-        text: 'Compass Rose School Teacher'
-    },
-    {
-        value: 'cratherapist.template',
-        text: 'Therapist'
-    }
-]
-
-const crownPointJobs = [
-    {
-        value: 'fscrownpoint.template',
-        text: 'Family Specialist'
-    },
-    {
-        value: 'fca.crownpoint',
-        text: 'Foster Care Advocate'
-    },
-    {
-        value: 'oa.crownpoint',
-        text: 'Office Assistant'
-    },
-    {
-        value: 'regionalmanager.crownpoint',
-        text: 'Regional Manager'
-    }]
-
-const fortWayneJobs = [
-    {
-        value: 'fsfortwayne.template',
-        text: 'Family Specialist'
-    },
-    {
-        value: 'fca.fortwayne',
-        text: 'Foster Care Advocate'
-    },
-    {
-        value: 'oa.fortwayne',
-        text: 'Office Assistant'
-    },
-    {
-        value: 'regionalmanager.fortwayne',
-        text: 'Regional Manager'
-    }]
-
-const greenwoodJobs = [
-    {
-        value: 'fsgreenwood.template',
-        text: 'Family Specialist'
-    },
-    {
-        value: 'fca.greenwood',
-        text: 'Foster Care Advocate'
-    },
-    {
-        value: 'oa.greenwood',
-        text: 'Office Assistant'
-    },
-    {
-        value: 'regionalmanager.greenwood',
-        text: 'Regional Manager'
-    }]
-
-const indianapolisJobs = [
-    {
-        value: 'fsindianapolis.template',
-        text: 'Family Specialist'
-    },
-    {
-        value: 'fca.indianapolis',
-        text: 'Foster Care Advocate'
-    },
-    {
-        value: 'oa.indianapolis',
-        text: 'Office Assistant'
-    },
-    {
-        value: 'regionalmanager.indianapolis',
-        text: 'Regional Manager'
-    }]
-
-const recoveryServicesJobs = [
-    {
-        value: 'recoveryrft.template',
-        text: 'Recovery Services Family Teacher'
-    },
-    {
-        value: 'recovery.therapist',
-        text: 'Recovery Therapist'
-    }]
-
-const southBendJobs = [
-    {
-        value: 'fssouthbend.template',
-        text: 'Family Specialist'
-    },
-    {
-        value: 'fca.southbend',
-        text: 'Foster Care Advocate'
-    },
-    {
-        value: 'oa.southbend',
-        text: 'Office Assistant'
-    },
-    {
-        value: 'regionalmanager.southbend',
-        text: 'Regional Manager'
-    }]
-
-const volunteersAndInternsJobs = [
-    {
-        value: 'fs.intern',
-        text: 'Intern (Family Specialist)'
-    },
-    {
-        value: 'residential.intern',
-        text: 'Intern (Residential)'
-    }
-]
-
-const wabashJobs = [
-    {
-        value: 'gtfl.manufacturingtrainer',
-        text: 'Advanced Manufacturing Trainer'
-    },
-    {
-        value: 'cafeteriastaff.template',
+        value: 'adTemplateAccountName',
         text: 'Cafeteria Staff'
     },
     {
-        value: 'cm.template',
-        text: 'Case Manager'
+        value: 'adTemplateAccountName',
+        text: 'Coffee Advocate'
     },
     {
-        value: 'custodian.template',
+        value: 'adTemplateAccountName',
         text: 'Custodian'
     },
     {
-        value: 'fswabash.template',
-        text: 'Family Specialist'
+        value: 'adTemplateAccountName',
+        text: 'Floor Manager'
     },
     {
-        value: 'familypres.therapist',
-        text: 'Family Preservation Therapist'
-    },
-    {
-        value: 'familypres.programmanager',
-        text: 'Family Preservation Program Manager'
-    },
-    {
-        value: 'fca.wabash',
-        text: 'Foster Care Advocate'
-    },
-    {
-        value: 'groundskeeper.template',
+        value: 'adTemplateAccountName',
         text: 'Groundskeeper'
     },
     {
-        value: 'ycs.template',
-        text: 'Night Shift/YCS'
+        value: 'adTemplateAccountName',
+        text: 'Night Shift'
     },
     {
-        value: 'rft.template',
-        text: 'Residential Family Teacher'
+        value: 'fadTemplateAccountName',
+        text: 'Program Manager'
     },
     {
-        value: 'rts.template',
-        text: 'Residential Teaching Specialist'
+        value: 'adTemplateAccountName',
+        text: 'Special Specialist'
     },
     {
-        value: 'residentialtherapist',
-        text: 'Residential Therapist'
+        value: 'adTemplateAccountName',
+        text: 'Spreadsheet Master'
     },
     {
-        value: 'sa.template',
-        text: 'Store Associate'
-    },
-    {
-        value: 'transporter.template',
-        text: 'Transporter'
+        value: 'adTemplateAccountName',
+        text: 'Trainer'
     }
 ]
+
+const regionalOfficeOneJobs = [
+    {
+        value: 'adTemplateAccountName',
+        text: 'Account Therapist'
+    },
+    {
+        value: 'adTemplateAccountName',
+        text: 'Front Desk'
+    },
+    {
+        value: 'adTemplateAccountName',
+        text: 'Regional Manager'
+    }
+]
+
+const regionalOfficeTwoJobs = [
+    {
+        value: 'adTemplateAccountName',
+        text: 'Business Administrator'
+    },
+    {
+        value: 'adTemplateAccountName',
+        text: 'Front Desk'
+    },
+    {
+        value: 'adTemplateAccountName',
+        text: 'Office Assistant'
+    },
+    {
+        value: 'adTemplateAccountName',
+        text: 'Regional Manager'
+    }]
+
+const RegionalOfficeThreeJobs = [
+    {
+        value: 'adTemplateAccountName',
+        text: 'Front End Developer'
+    },
+    {
+        value: 'adTemplateAccountName',
+        text: 'Sharepoint Guy'
+    },
+    {
+        value: 'adTemplateAccountName',
+        text: 'System Administrator'
+    },
+    {
+        value: 'adTemplateAccountName',
+        text: 'Regional Manager'
+    }]
+
+const RegionalOfficeFourJobs = [
+    {
+        value: 'adTemplateAccountName',
+        text: 'Business Architecht'
+    },
+    {
+        value: 'adTemplateAccountName',
+        text: 'Marketing Director'
+    },
+    {
+        value: 'adTemplateAccountName',
+        text: 'Regional Manager'
+    },
+    {
+        value: 'adTemplateAccountName',
+        text: 'Yacht Salesman'
+    }]
 
 const createJobTitleOption = function (job) {
     const jobTitleOption = document.createElement('option');
@@ -201,57 +124,33 @@ const createJobTitleOption = function (job) {
 
 
 const configureJobTitleMenu = function (officeSelectMenu, jobTitleSelectMenu) {
-    if (officeSelectMenu.value === 'Compass Rose Academy') {
+    if (officeSelectMenu.value === 'Main Office') {
         jobTitleSelectMenu.length = 0;
         for (let job of compassRoseJobs) {
             createJobTitleOption(job);
         }
     }
-    if (officeSelectMenu.value === 'Crown Point') {
+    if (officeSelectMenu.value === 'Regional Office One') {
         jobTitleSelectMenu.length = 0;
         for (let job of crownPointJobs) {
             createJobTitleOption(job);
         }
     }
-    if (officeSelectMenu.value === 'Fort Wayne') {
+    if (officeSelectMenu.value === 'Regional Office Two') {
         jobTitleSelectMenu.length = 0;
         for (let job of fortWayneJobs) {
             createJobTitleOption(job);
         }
     }
-    if (officeSelectMenu.value === 'Greenwood') {
+    if (officeSelectMenu.value === 'Regional Office Three') {
         jobTitleSelectMenu.length = 0;
         for (let job of greenwoodJobs) {
             createJobTitleOption(job);
         }
     }
-    if (officeSelectMenu.value === 'Indianapolis') {
+    if (officeSelectMenu.value === 'Regional Office Four') {
         jobTitleSelectMenu.length = 0;
         for (let job of indianapolisJobs) {
-            createJobTitleOption(job);
-        }
-    }
-    if (officeSelectMenu.value === 'Recovery Services') {
-        jobTitleSelectMenu.length = 0;
-        for (let job of recoveryServicesJobs) {
-            createJobTitleOption(job);
-        }
-    }
-    if (officeSelectMenu.value === 'South Bend') {
-        jobTitleSelectMenu.length = 0;
-        for (let job of southBendJobs) {
-            createJobTitleOption(job);
-        }
-    }
-    if (officeSelectMenu.value === 'Volunteers & Interns') {
-        jobTitleSelectMenu.length = 0;
-        for (let job of volunteersAndInternsJobs) {
-            createJobTitleOption(job);
-        }
-    }
-    if (officeSelectMenu.value === 'Wabash') {
-        jobTitleSelectMenu.length = 0;
-        for (let job of wabashJobs) {
             createJobTitleOption(job);
         }
     }
